5d5ed3fb9ad9084a3f2721e8670a4a0dddce60e6
Damien Plumettaz <damien.plumettaz@plumettaz.com>
2017-08-10 10:51:10 +0200

    add options for the middlewares, fix launch issue when the tests are disable during installation

--- templates/config/webpack.js
+++ templates/config/webpack.js
@@ -1,57 +1,71 @@
 const path = require('path');
 
+function requireIfExists(path, fallback) {
+  try {
+    return require(path);
+  }
+  catch(err) {
+    if(err.code === 'MODULE_NOT_FOUND') return fallback;
+    throw err;
+  }
+}
+
 module.exports = {
 	webpack: {
-        /***************************************************************************
-         *                                                                         *
-         * The webpack compilation should finished before this timeout or sails.js *
-         * will not start                                                          *
-         *                                                                         *
-         ***************************************************************************/
-        // _hookTimeout: 60000,
+  /***************************************************************************
+   *                                                                         *
+   * The webpack compilation should finished before this timeout or sails.js *
+   * will not start                                                          *
+   *                                                                         *
+   ***************************************************************************/
+  // _hookTimeout: 60000,
 
-        /***************************************************************************
-         *                                                                         *
-         * A prefix to add to the hook logs                                        *
-         *                                                                         *
-         ***************************************************************************/
-        // logPrefix: 'sails-hook-webpack2:',
+  /***************************************************************************
+   *                                                                         *
+   * A prefix to add to the hook logs                                        *
+   *                                                                         *
+   ***************************************************************************/
+  // logPrefix: 'sails-hook-webpack2:',
 
 
-        /***************************************************************************
-         *                                                                         *
-         * Options passed to webpack                                               *
-         *                                                                         *
-         ***************************************************************************/
+  /***************************************************************************
+   *                                                                         *
+   * Options passed to webpack                                               *
+   *                                                                         *
+   ***************************************************************************/
 		options: {
 			context: path.resolve(__dirname, '../webpack/'),
 		},
 
-        /***************************************************************************
-         *                                                                         *
-         * Options for the middlewares used during development                     *
-         * - hot: is for (webpack-hot-middleware)                                  *
-         *                   [https://github.com/glenjamin/webpack-hot-middleware] *
-         * - dev: is for (webpack-dev-middleware)                                  *
-         *                     [https://github.com/webpack/webpack-dev-middleware] *
-         * - history: is for (connect-history-api-fallback)                        *
-         *              [https://github.com/bripkens/connect-history-api-fallback] *
-         *                                                                         *
-         ***************************************************************************/
-        middlewares: {
-          hot: { quiet: true },
-          dev: { quiet: false },
-          history: {}
-        },
+    /***************************************************************************
+     *                                                                         *
+     * Options for the middlewares used during development                     *
+     * - after: the name of the middleware after which to insert the           *
+     *          middlewares                                                    *
+     * - hot: will be passed to the (webpack-hot-middleware)                   *
+     *                   [https://github.com/glenjamin/webpack-hot-middleware] *
+     * - dev: will be passed to the (webpack-dev-middleware)                   *
+     *                     [https://github.com/webpack/webpack-dev-middleware] *
+     * - history:will be passed to the (connect-history-api-fallback)          *
+     *              [https://github.com/bripkens/connect-history-api-fallback] *
+     *   when history is falsy, the history middleware will not be inserted    *
+     *                                                                         *
+     ***************************************************************************/
+    middlewares: {
+      // after: 'router',
+      hot: { quiet: true },
+      dev: { quiet: false },
+      history: {}
+    },
 
 
-        /***************************************************************************
-         *                                                                         *
-         * This get merged with the webpack options by the hook when sails lift    *
-         *                                                                         *
-         ***************************************************************************/
+    /***************************************************************************
+     *                                                                         *
+     * This get merged with the webpack options by the hook when sails lift    *
+     *                                                                         *
+     ***************************************************************************/
 		production: require('../webpack/build/webpack.prod.conf.js'),
 		development: require('../webpack/build/webpack.dev.conf.js'),
-        testing: require('../webpack/build/webpack.test.conf.js')
+    testing: requireIfExists('../webpack/build/webpack.test.conf.js')
 	}
 }
